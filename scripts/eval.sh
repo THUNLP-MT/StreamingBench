bed1fce94efd4954316f16a7d437db9eca0d3e20
Junming Lin <114148730+mjuicem@users.noreply.github.com>
2025-02-15 04:52:57 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -5,7 +5,7 @@ cd ../src
 EVAL_MODEL="MiniCPM-V"
 Devices=0
 
-# For real-time visual understanding 
+# For real-time visual understanding(Offline + Text Instruction)
 
 TASK="real"
 DATA_FILE="./data/questions_${TASK}.json"
@@ -17,7 +17,7 @@ if [ "$EVAL_MODEL" = "MiniCPM-V" ]; then
     CUDA_VISIBLE_DEVICES=$Devices python eval.py --model_name $EVAL_MODEL --benchmark_name $BENCHMARK --data_file $DATA_FILE --output_file $OUTPUT_FILE
 fi
 
-# For omni-source understanding
+# For omni-source understanding(Offline + Text Instruction)
 
 TASK="omni"
 DATA_FILE="./data/questions_${TASK}.json"
@@ -29,7 +29,7 @@ if [ "$EVAL_MODEL" = "MiniCPM-V" ]; then
     CUDA_VISIBLE_DEVICES=$Devices python eval.py --model_name $EVAL_MODEL --benchmark_name $BENCHMARK --data_file $DATA_FILE --output_file $OUTPUT_FILE
 fi
 
-# For sequential question answering
+# For sequential question answering(Offline + Text Instruction)
 
 TASK="sqa"
 DATA_FILE="./data/questions_${TASK}.json"
@@ -40,12 +40,25 @@ if [ "$EVAL_MODEL" = "MiniCPM-V" ]; then
     conda activate MiniCPM-V
     CUDA_VISIBLE_DEVICES=$Devices python eval.py --model_name $EVAL_MODEL --benchmark_name $BENCHMARK --data_file $DATA_FILE --output_file $OUTPUT_FILE
 fi
-# For proactive output
+# For proactive output(Offline + Text Instruction)
 
 TASK="proactive"
 DATA_FILE="./data/questions_${TASK}.json"
 OUTPUT_FILE="./data/${TASK}_output_${EVAL_MODEL}.json"
 BENCHMARK="StreamingProactive"
+
+if [ "$EVAL_MODEL" = "MiniCPM-V" ]; then
+    conda activate MiniCPM-V
+    CUDA_VISIBLE_DEVICES=$Devices python eval.py --model_name $EVAL_MODEL --benchmark_name $BENCHMARK --data_file $DATA_FILE --output_file $OUTPUT_FILE
+fi
+
+# (Streaming/Online + Text Instruction)
+# Optional Task(real, omni, sqa)
+
+TASK="real"
+DATA_FILE="./data/questions_${TASK}_stream.json"
+OUTPUT_FILE="./data/${TASK}_text_stream_output_${EVAL_MODEL}.json"
+BENCHMARK="StreamingOpenStreamText"
 
 if [ "$EVAL_MODEL" = "MiniCPM-V" ]; then
     conda activate MiniCPM-V
